efd8dfe97b6bc11524685c65c7fd5990bcf6183c
Pawissakan Chirupphapa <pawissakan.cpcu@gmail.com>
2023-05-04 18:28:15 +0700

    feat: add sync wave to apps

--- scripts/install_argo.sh
+++ scripts/install_argo.sh
@@ -3,3 +3,9 @@
 ROOT_DIR=$PWD/..
 
 kubectl apply -k $ROOT_DIR/k8s/argocd
+
+# Wait until ArgoCD CRDs are available.
+sleep 2
+
+# Install root Application
+kubectl apply -k $ROOT_DIR/k8s/argocd/bootstrap
